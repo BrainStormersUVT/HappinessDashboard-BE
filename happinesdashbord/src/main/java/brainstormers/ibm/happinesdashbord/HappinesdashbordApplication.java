ce656155a1b5371c5928398d488aa63224ba5666
XXMihaiXX969 <mihai.gherghinescu15@gmail.com>
2022-04-25 12:25:40 +0300

    first commit

---
+++ happinesdashbord/src/main/java/brainstormers/ibm/happinesdashbord/HappinesdashbordApplication.java
@@ -0,0 +1,13 @@
+package brainstormers.ibm.happinesdashbord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappinesdashbordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappinesdashbordApplication.class, args);
+	}
+
+}
---
+++ happinesdashbord/src/test/java/brainstormers/ibm/happinesdashbord/HappinesdashbordApplicationTests.java
@@ -0,0 +1,13 @@
+package brainstormers.ibm.happinesdashbord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappinesdashbordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
